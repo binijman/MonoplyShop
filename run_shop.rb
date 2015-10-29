3bbde6f23931fe667e3989e1fd8028d90909afc5
Bert <bertnijman@MacBook-Pro.local>
2015-10-29 17:08:07 +0100

    versie 1.0

--- run_shop.rb
+++ run_shop.rb
@@ -35,18 +35,31 @@ list_cards = []
 
 # system ('clear')
 
-#-Intro + playername
+# - Intro + playername
 puts "Welcome on the street-shop Online!\n\nWhat is your name?"
 player = gets.chomp
 puts "Here you can buy your own street and create revenue from tennants!\n\n"
 
+# - Let player buy property cards (as many as he would like)
 answer = "y"
 while answer == "y"
   buy_property(database,player)
   puts "Would you like to buy another property? (y/n)"
   answer = gets.chomp
 end
-#
-# database.each do |street|
-#   puts "Rent for #{street.name} = € #{street.rent(set)}"
-# end
+puts "Thank you for using our store!\nThe bought streets are now your property! "
+
+# - Display list of properties in possesion of Player
+player_cards = []  ;  i = 0 ; total = 0 ; revenue = 0
+#-List player cards
+  puts "\nThese are you purchases:"
+  database.each do |property|
+      if property.owner == player
+          puts "#{i+1} - € #{property.price}\t€ #{property.revenue} revenue per month,\t#{property.name}"
+          player_cards << property.name
+          i += 1
+          total += property.price
+          revenue += property.revenue
+      end
+  end
+puts "\nThe total value of your estate is € #{total}, and your can expect € #{revenue} revenue per month."
