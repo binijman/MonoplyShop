94eea68480183b885ec07da0ec77c2bbbc1539b3
Bert <bertnijman@MacBook-Pro.local>
2015-10-29 14:39:28 +0100

    versie 0.3

--- run_shop.rb
+++ run_shop.rb
@@ -1,23 +1,23 @@
 require "./classes.rb"
 require "./data.rb"
 
-st1 = Station.new('Noord')
-st2 = Station.new('Zuid')
-
-set = 3
-brink = Street.new('Brink',100,12,3,'Utrecht',"Orange")
-
-puts "Rent for #{brink.name} = € #{brink.rent(set)}"
-
-set = 4
-puts "Rent for #{st1.name} = € #{st1.rent(set)}"
-
-collection = StreetCollection.new()
-streets = collection.streets
+# st1 = Station.new('Noord')
+# st2 = Station.new('Zuid')
+#
+# set = 3
+# brink = Street.new('Brink',100,12,3,'Utrecht',"Orange")
+#
+# puts "Rent for #{brink.name} = € #{brink.rent(set)}"
+#
+# set = 4
+# puts "Rent for #{st1.name} = € #{st1.rent(set)}"
+#
+# collection = StreetCollection.new()
+# streets = collection.streets
 
 # create database var -> array[obj]
 database = fill_database()
-
+set = 3
 database.each do |street|
   puts "Rent for #{street.name} = € #{street.rent(set)}"
 
